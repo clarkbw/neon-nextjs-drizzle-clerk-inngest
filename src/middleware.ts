29113aa2757678cd0687fb7400fceb2fe18ec948
Bryan Clark <clarkbw@gmail.com>
2024-07-21 12:08:00 -0700

    new matcher and cool route protected

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,16 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// Define routes that should be protected
-const isProtectedRoute = createRouteMatcher([
-  '/' // Add any additional routes here
-]);// Update clerkMiddleware to manually protect routes
+const isProtectedRoute = createRouteMatcher(["/cool"]);
 
 export default clerkMiddleware((auth, req) => {
-  // if (isProtectedRoute(req)) {
-  //   auth().protect(); // Protect the route if it matches the defined criteria
-  // }
-}, { debug: true });
+  if (isProtectedRoute(req)) auth().protect();
+});
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    // Skip all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for api routes
+    "/(api|trpc)(.*)",
+  ],
 };
